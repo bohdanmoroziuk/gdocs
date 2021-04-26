c2d510486312a577d272b1f1fbcf9e1aa687116c
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-04-26 11:13:29 +0300

    feat(client.text-editor): customize toolbar

--- client/src/components/TextEditor/index.js
+++ client/src/components/TextEditor/index.js
@@ -3,6 +3,18 @@ import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import 'components/TextEditor/styles.css';
 
+const toolbarOptions = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
+
 const TextEditor = () => {
   const wrapperRef = useCallback((wrapper) => {
     if (!wrapper) return;
@@ -14,6 +26,9 @@ const TextEditor = () => {
 
     new Quill(editor, {
       theme: 'snow',
+      modules: {
+        toolbar: toolbarOptions,
+      },
     });
   }, []);
 
